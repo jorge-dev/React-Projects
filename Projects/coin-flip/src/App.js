4cb00af197bdd26bd6b4e53e82a77368b226ae27
jorge-dev <jo.luisa94@gmail.com>
2019-06-28 01:59:41 -0600

    finished coin flip

--- Projects/coin-flip/src/App.js
+++ Projects/coin-flip/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import CoinContainer from "./CoinContainer";
 import "./App.css";
 
 function App() {
-  return <div className="App">a</div>;
+  return (
+    <div className="App">
+      <CoinContainer />
+    </div>
+  );
 }
 
 export default App;
