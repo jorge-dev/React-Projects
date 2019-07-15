05d1cf62c4a3ddabf670ecddb413cb3e78d03fd0
jorge-dev <jo.luisa94@gmail.com>
2019-07-14 22:10:39 -0600

    todo app

---
+++ TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class TodoList extends Component {
+    constructor
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TodoList
