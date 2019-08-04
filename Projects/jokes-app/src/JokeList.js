006b041dd4ffe2267af430bf140e9242c478e767
jorge-dev <jo.luisa94@gmail.com>
2019-08-04 00:27:40 -0600

    created joke and updated jokelist component

--- Projects/jokes-app/src/JokeList.js
+++ Projects/jokes-app/src/JokeList.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./JokeList.css";
+import Joke from "./Joke";
+import uuid from "uuid/v4";
 export default class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10
@@ -10,6 +13,15 @@ export default class JokeList extends Component {
     this.state = {
       jokes: []
     };
+    this.handleVote = this.handleVote.bind(this);
+  }
+
+  handleVote(id, change) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + change } : j
+      )
+    }));
   }
 
   async componentDidMount() {
@@ -18,7 +30,7 @@ export default class JokeList extends Component {
       let response = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json" }
       });
-      jokes.push(response.data.joke);
+      jokes.push({ id: uuid(), text: response.data.joke, votes: 0 });
     }
 
     this.setState({ jokes: jokes });
@@ -26,10 +38,27 @@ export default class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Jokes</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title">
+            {" "}
+            <span>Dad </span>Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt="laughing emoji"
+          />
+          <button className="JokeList-loadMore">New Jokes</button>
+        </div>
+
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <Joke
+              upVote={() => this.handleVote(j.id, 1)}
+              downVote={() => this.handleVote(j.id, -1)}
+              id={j.id}
+              votes={j.votes}
+              text={j.text}
+            />
           ))}
         </div>
       </div>
