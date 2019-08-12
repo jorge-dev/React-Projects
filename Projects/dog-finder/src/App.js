554a031f5d7e3ffe67e7a10fed7a779abdd13583
jorge-dev <jo.luisa94@gmail.com>
2019-08-12 11:10:50 -0600

    dog app created

---
+++ Projects/dog-finder/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+import "./App.css";
+import Navbar from "./Navbar";
+
+Class App extends Component {
+  render() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="display-1">Dog Finder</h1>
+    </div>
+  );
+  }
+}
+
+export default App;
