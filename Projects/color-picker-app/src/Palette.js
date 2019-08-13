d75c151fccdc12011b58074eee1497ef863143e5
jorge-dev <jo.luisa94@gmail.com>
2019-08-13 09:52:14 -0600

    created color picker app and Palette component

---
+++ Projects/color-picker-app/src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colours">{/* Color boxes */}</div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
