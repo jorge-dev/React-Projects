84cb36213ea20e64729e70cf172d14adbc732bad
jorge-dev <jo.luisa94@gmail.com>
2019-08-13 10:32:20 -0600

    added some styles for the ColorBox component

--- Projects/color-picker-app/src/ColorBox.js
+++ Projects/color-picker-app/src/ColorBox.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 export default class ColorBox extends Component {
   render() {
+    const { background, name } = this.props;
     return (
       <div className="ColorBox" style={{ background: this.props.background }}>
-        <span>{this.props.name}</span>
-        <span>More</span>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
