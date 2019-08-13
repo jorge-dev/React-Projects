5749717e276027e0e2c4d3092043a75d98e8ae20
jorge-dev <jo.luisa94@gmail.com>
2019-08-13 10:09:47 -0600

    added ColorBox component and some basic css for testing

--- Projects/color-picker-app/src/App.js
+++ Projects/color-picker-app/src/App.js
@@ -6,7 +6,7 @@ import seedColors from "./seedColors";
 function App() {
   return (
     <div className="App">
-      <Palette colours={...seedColors[5]} />
+      <Palette {...seedColors[3]} />
     </div>
   );
 }
--- Projects/color-picker-app/src/Palette.js
+++ Projects/color-picker-app/src/Palette.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-
+import ColorBox from "./ColorBox";
+import "./Palette.css";
 export default class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
       <div className="Palette">
         {/* Navbar goes here */}
-        <div className="Palette-colours">{/* Color boxes */}</div>
+        <div className="Palette-colors">{colorBoxes}</div>
         {/* footer */}
       </div>
     );
