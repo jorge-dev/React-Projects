524e5935bcb8b83cde3da4735e7449b1b6dc1ee6
jorge-dev <jo.luisa94@gmail.com>
2019-08-13 22:05:32 -0600

    added some more styles for the color slider and

--- Projects/color-picker-app/src/Palette.js
+++ Projects/color-picker-app/src/Palette.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+
 export default class Palette extends Component {
   constructor(props) {
     super(props);
@@ -25,13 +26,15 @@ export default class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          onAfterChange={this.changeLevel}
-          step={100}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            onAfterChange={this.changeLevel}
+            step={100}
+          />
+        </div>
         {/* Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer */}
