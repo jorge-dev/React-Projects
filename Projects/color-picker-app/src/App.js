328913c78177dd6d8600b297c70d897995176939
jorge-dev <jo.luisa94@gmail.com>
2019-08-13 21:55:10 -0600

    added simple color shade slider

--- Projects/color-picker-app/src/App.js
+++ Projects/color-picker-app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[3]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
