5ffaec43466e70e7f05b3a9fabc98ebd19f64a14
jorge-dev <jo.luisa94@gmail.com>
2019-08-16 11:10:15 -0600

    some minor fixes to route

--- Projects/color-picker-app/src/App.js
+++ Projects/color-picker-app/src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>Palette List oes here</h1>} />
-      <Route
-        exact
-        path="/palette/:id"
-        render={() => <h1>Individual oes here</h1>}
-      />
-    </Switch>
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
 
-    // <div className="App">
-    //   <Palette palette={generatePalette(seedColors[4])} />
-    // </div>
-  );
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List oes here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
