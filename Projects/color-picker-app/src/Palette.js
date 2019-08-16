40299898d8d9ccd3aa8fb79b76ee0f0e51dd67e1
jorge-dev <jo.luisa94@gmail.com>
2019-08-16 10:54:29 -0600

    added a footer

--- Projects/color-picker-app/src/Palette.js
+++ Projects/color-picker-app/src/Palette.js
@@ -24,11 +24,11 @@ export default class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox key={color.id} background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
@@ -37,9 +37,12 @@ export default class Palette extends Component {
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
         />
-        {/* Navbar goes here */}
+
         <div className="Palette-colors">{colorBoxes}</div>
-        {/* footer */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
