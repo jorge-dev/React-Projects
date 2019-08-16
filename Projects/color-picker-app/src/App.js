8df252be409f76ba823a0fcb1b4edf0c3098cabc
jorge-dev <jo.luisa94@gmail.com>
2019-08-16 11:01:04 -0600

    Added react Router

--- Projects/color-picker-app/src/App.js
+++ Projects/color-picker-app/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette List oes here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>Individual oes here</h1>}
+      />
+    </Switch>
+
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
