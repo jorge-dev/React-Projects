3efeaad8231b777a5375373537f49c2bee7f0f2d
jorge-dev <jo.luisa94@gmail.com>
2019-08-29 20:31:22 -0600

    added some other stuff

--- Projects/color-picker-app/src/ColorBox.js
+++ Projects/color-picker-app/src/ColorBox.js
@@ -1,50 +1,51 @@
 import React, { Component } from "react";
-import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
-export default class ColorBox extends Component {
+import chroma from "chroma-js";
+import "./ColorBox.css";
+
+class ColorBox extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      copied: false
-    };
+    this.state = { copied: false };
     this.changeCopyState = this.changeCopyState.bind(this);
   }
-
   changeCopyState() {
     this.setState({ copied: true }, () => {
       setTimeout(() => this.setState({ copied: false }), 1500);
     });
   }
-
   render() {
-    const { background, name, paletteId, id, showLink } = this.props;
+    const { name, background, moreUrl, showLink } = this.props;
     const { copied } = this.state;
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div className="ColorBox" style={{ background: this.props.background }}>
+        <div style={{ background }} className="ColorBox">
           <div
             style={{ background }}
             className={`copy-overlay ${copied && "show"}`}
           />
           <div className={`copy-msg ${copied && "show"}`}>
-            <h1>copied</h1>
-            <p>{this.props.background}</p>
+            <h1>copied!</h1>
+            <p className={isLightColor && "dark-text"}>
+              {this.props.background}
+            </p>
           </div>
-
           <div className="copy-container">
             <div className="box-content">
-              <span>{name}</span>
+              <span className={isDarkColor && "light-text"}>{name}</span>
             </div>
-            <button className="copy-button">Copy</button>
+            <button className={`copy-button ${isLightColor && "dark-text"}`}>
+              Copy
+            </button>
           </div>
           {showLink && (
-            <Link
-              to={`/palette/${paletteId}/${id}`}
-              onClick={e => e.stopPropagation()}
-            >
-              <span className="see-more">More</span>
+            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+              <span className={`see-more ${isLightColor && "dark-text"}`}>
+                MORE
+              </span>
             </Link>
           )}
         </div>
@@ -52,3 +53,4 @@ export default class ColorBox extends Component {
     );
   }
 }
+export default ColorBox;
--- Projects/color-picker-app/src/Palette.js
+++ Projects/color-picker-app/src/Palette.js
@@ -1,40 +1,31 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-
-import "./Palette.css";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
+import "./Palette.css";
 
-export default class Palette extends Component {
+class Palette extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      level: 500,
-      format: "hex"
-    };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
     this.changeFormat = this.changeFormat.bind(this);
   }
-
   changeLevel(level) {
     this.setState({ level });
   }
-
   changeFormat(val) {
     this.setState({ format: val });
   }
   render() {
     const { colors, paletteName, emoji, id } = this.props.palette;
     const { level, format } = this.state;
-
     const colorBoxes = colors[level].map(color => (
       <ColorBox
-        key={color.id}
         background={color[format]}
         name={color.name}
-        id={color.id}
-        paletteId={id}
+        key={color.id}
+        moreUrl={`/palette/${id}/${color.id}`}
         showLink
       />
     ));
@@ -44,11 +35,12 @@ export default class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showingAllColors
         />
-
         <div className="Palette-colors">{colorBoxes}</div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
 }
+export default Palette;
