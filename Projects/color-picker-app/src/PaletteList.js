9efdd38e70cd0eb5e6174f78c83debd95bdc5b4e
jorge-dev <jo.luisa94@gmail.com>
2019-09-03 09:39:34 -0600

    refactor styles to its own files

--- Projects/color-picker-app/src/PaletteList.js
+++ Projects/color-picker-app/src/PaletteList.js
@@ -2,36 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
 import { makeStyles, withStyles, withTheme } from "@material-ui/styles";
-
-const css = {
-  root: {
-    backgroundColor: "blue",
-    height: "100vh",
-    display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "center"
-  },
-  container: {
-    width: "50%",
-    display: "flex",
-    alignItems: "flex-start",
-    flexDirection: "column",
-    flexWrap: "wrap"
-  },
-  nav: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
-    color: "white"
-  },
-  palette: {
-    boxSizing: "border-box",
-    width: "100%",
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
-  }
-};
+import styles from "./Styles/PaletteListStyles";
 
 class PaletteList extends Component {
   goToPalette(id) {
@@ -56,4 +27,4 @@ class PaletteList extends Component {
   }
 }
 
-export default withStyles(css)(PaletteList);
+export default withStyles(styles)(PaletteList);
