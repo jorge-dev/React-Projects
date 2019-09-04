6ef897d6d5377efd78a913bf1868bd9f1510ca52
jorge-dev <jo.luisa94@gmail.com>
2019-09-04 13:53:13 -0600

    Added save palette functionality

--- Projects/color-picker-app/src/NewPaletteForm.js
+++ Projects/color-picker-app/src/NewPaletteForm.js
@@ -88,6 +88,7 @@ class NewPaletteForm extends Component {
     this.UpdateCurrColor = this.UpdateCurrColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -119,6 +120,17 @@ class NewPaletteForm extends Component {
     this.setState({ newName: e.target.value });
   }
 
+  handleSubmit() {
+    let newName = "new Test Palette";
+    const newPalette = {
+      paletteName: newName,
+      id: newName.toLocaleLowerCase().replace(/ /g, "-"),
+      colors: this.state.colors
+    };
+    this.props.savePalette(newPalette);
+    this.props.history.push("/");
+  }
+
   handleDrawerOpen = () => {
     this.setState({ open: true });
   };
@@ -136,6 +148,7 @@ class NewPaletteForm extends Component {
         <CssBaseline />
         <AppBar
           position="fixed"
+          color="default"
           className={classNames(classes.appBar, {
             [classes.appBarShift]: open
           })}
@@ -152,6 +165,13 @@ class NewPaletteForm extends Component {
             <Typography variant="h6" color="inherit" noWrap>
               Persistent drawer
             </Typography>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.handleSubmit}
+            >
+              Save Palette
+            </Button>
           </Toolbar>
         </AppBar>
         <Drawer
