e83cbaa69344d2e9e97809920363545942a934dd
jorge-dev <jo.luisa94@gmail.com>
2019-09-04 12:55:45 -0600

    Created draggable color box (without drag functionality)

--- Projects/color-picker-app/src/NewPaletteForm.js
+++ Projects/color-picker-app/src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
 import { ChromePicker } from "react-color";
+import DraggableColorBox from "./DraggableColorBox";
 
 const drawerWidth = 400;
 
@@ -55,6 +56,7 @@ const styles = theme => ({
     justifyContent: "flex-end"
   },
   content: {
+    height: "calc(100vh - 64px)",
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
     transition: theme.transitions.create("margin", {
@@ -172,11 +174,9 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
-          <ul>
-            {this.state.colors.map(color => (
-              <li>{color}</li>
-            ))}
-          </ul>
+          {this.state.colors.map(c => (
+            <DraggableColorBox color={c} />
+          ))}
         </main>
       </div>
     );
